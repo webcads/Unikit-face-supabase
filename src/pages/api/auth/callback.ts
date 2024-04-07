30bb5980739239bcdf9c053d16484bdeee676391
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2024-04-07 14:04:24 -0400

    ath with google ans insert a new user using meta datas

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -17,8 +17,9 @@ export const GET: APIRoute = async ({ url, cookies, redirect }) => {
 
   const { access_token, refresh_token } = data.session;
   const { user } = data;
-   // Validar si el correo electrónico tiene la extensión @unicauca.edu.co
-   if (user.email && !user.email.endsWith('@unicauca.edu.co')) {
+
+  // Validar si el correo electrónico tiene la extensión @unicauca.edu.co
+  if (user.email && !user.email.endsWith('@unicauca.edu.co')) {
     return new Response("Solo se permiten correos electrónicos de la Universidad del Cauca (@unicauca.edu.co)", { status: 403 });
   }
 
@@ -28,6 +29,61 @@ export const GET: APIRoute = async ({ url, cookies, redirect }) => {
   cookies.set("sb-refresh-token", refresh_token, {
     path: "/",
   });
+  
+  // Mostrar metadatos del usuario por consola
+  console.log("Metadatos del usuario:", user.user_metadata);
+
+  // Dividir el full_name en nombres y apellidos
+  const fullName = user.user_metadata?.full_name;
+  let nombres = '';
+  let apellidos = '';
+
+  if (fullName) {
+    const parts = fullName.split(' ');
+    nombres = parts.slice(0, -2).join(' ');
+    apellidos = parts.slice(-2).join(' ');
+  }
+
+  // Verificar si ya existe un usuario con el mismo uid_auth en la tabla usuario
+  const { data: existingUsers, error: userError } = await supabase
+    .from('usuario')
+    .select('*')
+    .eq('uid_auth', user.id);
+
+  if (userError) {
+    console.error("Error al buscar usuarios:", userError);
+    // Manejar el error apropiadamente
+    return new Response("Error al buscar usuarios", { status: 500 });
+  }
+
+  // Si ya existe un usuario con el mismo uid_auth, no es necesario insertar un nuevo registro
+  if (existingUsers && existingUsers.length > 0) {
+    console.log("El usuario ya existe en la tabla usuario. Saltando la inserción.");
+  } else {
+    // Insertar más campos del usuario en la tabla usuario
+    const { data: userData, error: insertError } = await supabase
+      .from('usuario')
+      .insert([
+        { 
+          uid_auth: user.id,
+          picture: user.user_metadata?.picture, 
+          full_name: fullName,
+          nombres: nombres,
+          apellidos: apellidos,
+          identificacion: user.user_metadata?.identificacion,
+          tipo_sangre: user.user_metadata?.tipo_sangre,
+          correo_unicauca: user.email
+        }
+      ]);
+
+    if (insertError) {
+      console.error("Error al insertar los datos del usuario:", insertError);
+      // Manejar el error apropiadamente
+      return new Response("Error al insertar los datos del usuario", { status: 500 });
+    } else {
+      console.log("Usuario insertado correctamente en la tabla usuario.");
+    }
+  }
 
   return redirect("/api/dashboard");
 };
