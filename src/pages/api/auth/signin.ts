9cbd2d1a3dd7ff36a9458fca3b49ce386c939f24
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2024-04-08 15:43:10 -0400

    Update signin.ts

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -19,7 +19,9 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: provider as Provider,
       options: {
-        redirectTo: "https://lucky-meringue-8b3000.netlify.app/api/auth/callback"
+        redirectTo: import.meta.env.SUPABASE_URL
+        ? "http://localhost:4321/api/auth/callback"
+        : "https://lucky-meringue-8b3000.netlify.app/api/auth/callback",
       },
     });
 
